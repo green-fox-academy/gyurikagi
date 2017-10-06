36e310cb190e0039e50dccf4cfa2e19c0c9506a4
gyurikagi <agnes.gyurik@gmail.com>
2017-10-06 13:35:32 +0200

    triangle ended

---
+++ week-01/day-4/triangle/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*create a simple program which generates a triangle like this:
+1
+22
+333
+4444
+55555
+666666
+7777777
+if you change the rows variable the output should be different as well
+*/
+int main()
+{
+    int rows = 0;
+
+    printf("Please add the number of rows\n");
+    scanf("%d", &rows);
+    printf("\n");
+
+    for (int i = 1; i <= rows; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
