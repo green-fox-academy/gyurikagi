eab4acf43b5e9297f82180b28769093699958ba9
gyurikagi <agnes.gyurik@gmail.com>
2017-10-11 17:25:11 +0200

    2 largest number in an array

--- week-02/day-2/advanced2/main.c
+++ week-02/day-2/advanced2/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <limits.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+	int number_array[8] = {48, 59, 2, -8, 55, 56, 78, 12};
+    int max = INT_MIN;
+    int max2 = INT_MIN;
+
+    for (int i = 0; i < 7; i++)
+    {
+         if (max < number_array [i]) {
+            max2 = max;
+            max = number_array [i];
+         }
+    }
+    printf("The largest element is %d, second largest is %d\n", max, max2);
+	//TODO:
+	// Write a C program to find the two largest element in an array using only 1 for loop
+	// From <limits.h> use INT_MIN: this is the least integer
+
+	return 0;
 }
