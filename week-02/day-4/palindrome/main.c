22dc9ac0601f4b911a9695015a75578fa73f81ee
gyurikagi <agnes.gyurik@gmail.com>
2017-10-12 14:27:02 +0200

    build a palindrome- draft of steps

---
+++ week-02/day-4/palindrome/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    // Give me an expression
+
+    //hold this expression in an array - named expr
+
+    // make new expression - in new_expr
+    // for length of expr new_expr [] = expr [], after that new_exp [length+i] = exp [length-i], i++
+
+    //print new exp
+
+
+    return 0;
+}
