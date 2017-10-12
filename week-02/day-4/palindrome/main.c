387dbf56ff51b38f87af62d94c3d685ef6d9fdb8
gyurikagi <agnes.gyurik@gmail.com>
2017-10-12 15:00:59 +0200

    Palindrome bulóilder ended

--- week-02/day-4/palindrome/main.c
+++ week-02/day-4/palindrome/main.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+    char expr[20];
+    int index = 0;
+
     // Give me an expression
+    printf("Give me an expression! (max length:20 character) \n");
 
     //hold this expression in an array - named expr
+    char character = getchar();
+    while (character != '\n') {
+		expr[index] = character;
+		index++;
+		character = getchar();
+	}
+	expr[index] = 0;
+    // print exp, and exp backwards
+    printf("Polindrome is the next:\n");
 
-    // make new expression - in new_expr
-    // for length of expr new_expr [] = expr [], after that new_exp [length+i] = exp [length-i], i++
+    int length = strlen(expr);
+    for (int i = 0; i < length; ++i) {
+        printf("%c", expr[i]);
+    }
+    for (int j = length-1; j >= 0; --j) {
+        printf("%c", expr[j]);
+    }
+    printf("\n");
 
     //print new exp
 
