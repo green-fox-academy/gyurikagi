c930a8a864a1e913dc2225aabdac66191860cacf
gyurikagi <agnes.gyurik@gmail.com>
2017-10-13 13:55:39 +0200

    Josephus problem

---
+++ week-02/day-4/Josephus_problem/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{   int number_of_solders = 0;
+
+    printf("Give me the number of people:\n");
+    scanf("%d", &number_of_solders);
+    int i = 0;
+    //int index = 0;
+
+    do {
+        pow(2, i);
+        i++;
+    } while ( pow(2, i) <= number_of_solders);
+
+   // int winner = (number_of_solders - index)*2+1;
+    if (pow(2, i-1) == number_of_solders) {
+        printf("The survivor is the first one");
+    } else {
+        int winner = (number_of_solders - pow(2, i-1))*2+1;
+        printf("The survivor is %d", winner);}
+
+    return 0;
+}
