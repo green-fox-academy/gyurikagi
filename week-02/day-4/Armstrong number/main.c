5f2ef8502db542910503ceba1dcbe98f4bcde641
gyurikagi <agnes.gyurik@gmail.com>
2017-10-14 00:52:09 +0200

    Armstrong number ended

--- week-02/day-4/Armstrong number/main.c
+++ week-02/day-4/Armstrong number/main.c
@@ -2,33 +2,35 @@
 #include <stdlib.h>
 #include <math.h>
 
-static double armstrong = 0;
-
 int main()
 {
     int number  = 0;
     int index = 0;
-    double helper = 0;
+    int helper = 0;
+    int helper2 = 0;
+    double armstrong = 0;
     // give me a number
     printf("Give me a number!\n");
     scanf("%d", &number);
 
-    // count caharacter in this number
+    // count characters in this number
     do {
         pow(10, index);
         index++;
     } while ( pow(10, index) <= number);
-    printf("Your number has %d digits", index);
+
     // calculate "Armstrong number"
 
-    for (int j = 0; j <= index; ++j)
+    for (int j = 0; j < index; ++j)
     {
-        helper = (number/pow(10, j))%10;
-        armstrong = pow(helper, index);
-
+        helper = (number/pow(10, j));
+        helper2 = helper%10;
+        armstrong = armstrong + pow(helper2, index);
     }
-
-
+    // compare your number with "armstrong" number
+        if (number == armstrong) {
+            printf("Your number is an Armstrong number");
+        } else printf("Your number is not an Armstrong number");
 
     return 0;
 }
