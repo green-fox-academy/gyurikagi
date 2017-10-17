769b88f3e05e4aa2d9024e5d15990bda59bdcf0e
gyurikagi <agnes.gyurik@gmail.com>
2017-10-17 13:40:24 +0200

    7c ended

--- week-03/day-2/6c/main.c
+++ week-03/day-2/6c/main.c
@@ -6,6 +6,11 @@ int main()
     float f_array[10] = {5.6, 8.73, 0.15, 0.99, 1.5, 6.3589, 10.01, -33.0, 0, 15};
     int i_array[10];
 
+    for (int i = 0; i < 10; i++) {
+        i_array[i] = f_array [i];
+        printf(" %d element of the array is %d\n", i, i_array[i]);
+    }
+
 	//TODO: write a program, which copies the values from the given array to an integer array
     //print out the elements of the integer array, with the "%d" format specifier
     //what do you see?
