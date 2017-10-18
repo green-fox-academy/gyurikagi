dd11b211fbf3ac397cb7c7b7cedf542f0cb0f10d
gyurikagi <agnes.gyurik@gmail.com>
2017-10-18 13:31:33 +0200

    scanf numbers without function

--- week-03/Új mappa/functions_for_calc/functions.c
+++ week-03/Új mappa/functions_for_calc/functions.c
@@ -21,5 +21,28 @@ Commands:\n\
  help	print usage\n\
  ==================================== \n\
  Hit enter to start!\n\
- ==================================== ");
+ ==================================== \n\
+This program only work with two numbers\n");
 }
+
+int give_numbers_and_operator (int a, int b, int c)
+{
+    int first_number (int a )
+    {scanf("%d", &a);
+        return a;}
+    int operator_needed ( int b){
+        scanf("%d", &b);
+        return b;}
+    int second_number (int c) {
+        scanf("%d", &c);
+    return c; }
+}
+
+//----------------------------------------------//
+
+void clear_screen ()
+{
+    system ("cls");
+}
+
+//---------------------------------------------//
--- week-03/Új mappa/functions_for_calc/main.c
+++ week-03/Új mappa/functions_for_calc/main.c
@@ -2,15 +2,41 @@
 #include <stdlib.h>
 #include "functions.h"
 
-
-
 int main()
 {
-    // printelje ki, amit ki kell - úgy nézzen ki, ahogy kell
+    int number1 = 0;
+    int number2 = 0;
+    int op = 0;
+
     start_screen();
+   // give_numbers_and_operator (number1, op, number2);
+    scanf("%d", &number1);
+    scanf("%d", &op);
+    scanf("%d", &number2);
+    printf("%d, %d, %d", number1, op, number2);
 
     // adatbekérés
 
+    // megnézni, melyik parancssort adta be és ez alapján döntési fa
+
     //clear paranccsal törlés
+
+    //szorzás
+    //osztás
+    //összeadás
+    //kivonás
+    //négyzetre emelés
+    // négyzetgyök
+    // maradékos osztás
+    // logaritmus
+    // binto - bináris -> decimálos
+    //hexto hexadec -> dec
+    // decto dec -> bin
+    // error handling
+        // nullával való osztás
+        // érvénytelen karakterek
+    // clear screen
+    // esc - lépjen ki a programból, de addig ne
+
     return 0;
 }
