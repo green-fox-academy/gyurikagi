8b20dbb6b122ef6ff84e750a2ead2a484b4525d2
gyurikagi <agnes.gyurik@gmail.com>
2017-10-25 17:43:54 +0200

    5c begin

--- week-04/day2/5c/main.c
+++ week-04/day2/5c/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
-    return 0;
+typedef struct house {
+    int address;
+    float price;
+    int rooms;
+    int area;
+}house1, house2, house3;
+// TODO:
+// Create a struct that represents a House
+// It should store:
+//  - The address of the house
+//  - The price in EUR
+//  - The number of rooms
+//  - The area of the house in square meters
+
+// TODO:
+// The market price of the houses is 400 EUR / square meters
+// Create a function that takes a pointer to a house and decides if it's worth to buy
+// (if the price is lower than the calculated price from it's area)
+
+// TODO:
+// Create a function that takes an array of houses (and it's length), and counts the
+// houses that are worth to buy.
+
+int main() {
+
+  return 0;
 }
