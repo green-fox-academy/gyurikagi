3bdae7db00c340197798736ddae99db05f840f02
gyurikagi <agnes.gyurik@gmail.com>
2017-11-02 11:38:06 +0100

    factoria by Bence- solved

--- week-04/Practice/function_practice_03/main.c
+++ week-04/Practice/function_practice_03/main.c
@@ -1,8 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*
+//---------------------------------------------------------------------------------------------------------------------
+write a function which calculate and print!! the factorial (n!) (define N to 5 at first, 5! is 120)
+//---------------------------------------------------------------------------------------------------------------------
+
+*/
+int factorial_with_recursion(int);
+
 int main()
-{
-    printf("Hello world!\n");
+{   int number = 0;
+    printf("Give me a number:");
+    scanf("%d", &number);
+    factorial_with_recursion(number);
+
     return 0;
+}
+
+int factorial_with_recursion(int num) {
+
+    int result = 1;   // show error if the user enters a negative integer
+    if (num > 0)
+        {
+            for (int c = 1; c <= num; c++ ) {
+                result = result * c;
+            }
+            printf("Factorial of %d = %d without recursion\n", num, result);
+        }
+       else {
+        printf("Error! Factorial of a negative number doesn't exist.");
+       }
+    return result;
 }
