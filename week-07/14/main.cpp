23e026f931996be5ff075d1da5bff1ee6b7e587f
gyurikagi <agnes.gyurik@gmail.com>
2017-11-20 17:20:09 +0100

    14 ended

--- week-07/14/main.cpp
+++ week-07/14/main.cpp
@@ -1,17 +1,25 @@
 #include <iostream>
 #include <ctime>
 #include <windows.h>
+#include <ctime>
 
 using namespace std;
 
 class Person {
-    Person()
+public:
+        Person(){
+        time_t now = time(0);
+        char *dt = ctime(&now);
+        cout << dt;
+
+    }
 };
 // Create a class named Person.
 // This class shoud have a constructor which prints out the
 // date of the creation of the istance.
 
 int main() {
+    Person p1;
 
-
+    return 0;
 }
