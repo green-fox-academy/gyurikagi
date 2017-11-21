48526bbdf09aab00f88d1e88c1e3a62c7a9e2fed
gyurikagi <agnes.gyurik@gmail.com>
2017-11-21 17:51:56 +0100

    06 task ended

---
+++ week-07/day-2/06/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Take the Integer class from the previous exercise and add two methods:
+// One which takes an Integer& parameter and adds the value of it to the member variable of this.
+// One which takes an int and adds the value of it to the member variable of this.
+//
+// Write code which uses them both and Prints out the results respectively.
+
+class Integer{
+private:
+    int num;
+public:
+    Integer(){
+        num = 0;
+    }
+
+    Integer(int a){
+        num = a;
+    }
+
+    int get(){
+        return num;
+    }
+
+
+    int add(int a){
+        num = num + a;
+        return get();
+
+    }
+
+
+    int add(Integer& a){
+        num = num + a.get();
+        return get();
+    }
+
+
+};
+
+
+
+int main() {
+
+    Integer a;
+    int b = 7;
+    Integer c(3);
+    Integer d(7);
+
+    cout << c.get() << endl;;
+    cout << a.get() << endl;;
+    cout << a.add(3) << endl;
+    cout << a.add(c) << endl;
+    cout << c.add(d) << endl;
+
+
+
+
+	return 0;
+}
